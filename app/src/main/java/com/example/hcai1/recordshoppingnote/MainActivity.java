d91688ad0c76a236fc9b45d1ded5f3cf5583ccc6
Hua Cai <hcai1@imach-43.cis.utas.edu.au>
2018-05-24 10:24:20 +1000

    first commit for setting up project

---
+++ app/src/main/java/com/example/hcai1/recordshoppingnote/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hcai1.recordshoppingnote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
